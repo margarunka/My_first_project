20f5a8a06e8a728e75a69dc816d7e68b9f0fbe08
margarunka <margarunka26@gmail.com>
2021-01-05 19:55:15 +0200

    Lesson 2: Функції додані

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 'use strict';
 
-const numberOfFilms = prompt("Сколько фильмов ви уже посмотрели?", " ");
+let numberOfFilms = prompt("Сколько фильмов ви уже посмотрели?");
+
+while (numberOfFilms == '' || numberOfFilms == null || numberOfFilms.length > 50) {
+      numberOfFilms = prompt("Сколько фильмов ви уже посмотрели?");
+}
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -12,14 +16,42 @@ const personalMovieDB = {
     ],
     privat: false
 };
-console.log(personalMovieDB.count);
+if (personalMovieDB.count <= 10) {
+    console.log("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count <= 30) {
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count > 30) {
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+// const a = prompt('Один из просмотрених фильмов?'),
+//       b = prompt('На сколько оцените его?'),
+//       c = prompt('Один из просмотрених фильмов?'),
+//       d = prompt('На сколько оцените его?');
 
-const a = prompt('Один из просмотрених фильмов?'),
-      b = prompt('На сколько оцените его?'),
-      c = prompt('Один из просмотрених фильмов?'),
-      d = prompt('На сколько оцените его?');
-personalMovieDB.movies = {
-    [a] : b,
-    [c] : d
-};
-console.log(personalMovieDB.movies);
+for (let i = 0; i < 2; i++) {
+    let a = prompt('Один из просмотрених фильмов?');
+    while(a == "" || a == null || a.length > 50) {
+        a = prompt('Один из просмотрених фильмов?');
+    }
+    let b = prompt('На сколько оцените его?');
+     
+    while(b == "" || b == null || b.length > 50) {
+        b = prompt('На сколько оцените его?');
+     }
+    personalMovieDB.movies[a] = b;
+}
+
+
+// let a = prompt('Один из просмотрених фильмов?');
+// let b = prompt('На сколько оцените его?');
+// do {
+//     a = prompt('Один из просмотрених фильмов?');
+// } while (a != "" &&   a != null && a.length > 50);
+
+
+// obj.key3 = "value3"; 
+
+
+console.log(personalMovieDB); 
